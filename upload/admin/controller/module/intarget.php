658db51cb38df6bb3c200119a6a76e2e2c2a8567
Gambit <chernozor@yandex.ru>
2015-11-09 19:46:52 +0300

    1.0.1 code refactor

--- upload/admin/controller/module/intarget.php
+++ upload/admin/controller/module/intarget.php
@@ -6,13 +6,13 @@
  * @copyright 2015 inTarget RU
  * @license   GNU General Public License, version 2
  */
-class ControllerModuleIntarget extends Controller
-{
+
+class ControllerModuleIntarget extends Controller {
     private $error = array();
+    private $name = "intarget";
     private $ver = '1.0.1';
 
-    public function index()
-    {
+    public function index() {
         $this->load->language('module/intarget');
         $this->document->setTitle($this->language->get('heading_title'));
         $this->load->model('setting/setting');
@@ -22,6 +22,12 @@ class ControllerModuleIntarget extends Controller
 
             $this->session->data['success'] = $this->language->get('text_success');
             $this->response->redirect($this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'));
+        }
+
+        if (isset($this->error['warning'])) {
+            $data['error_warning'] = $this->error['warning'];
+        } else {
+            $data['error_warning'] = '';
         }
 
         $data['heading_title'] = $this->language->get('heading_title');
@@ -70,10 +76,6 @@ class ControllerModuleIntarget extends Controller
             'href' => $this->url->link('module/intarget', 'token=' . $this->session->data['token'], 'SSL'),
         );
 
-        $data['action'] = $this->url->link('module/intarget', 'token=' . $this->session->data['token'], 'SSL');
-        $data['action_register'] = str_replace('&amp;', '&', $this->url->link('module/intarget/register', 'token=' . $this->session->data['token'], 'SSL'));
-        $data['cancel'] = $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL');
-
         $settings = $this->config->get('intarget');
 
         if (isset($this->request->post['email'])) {
@@ -103,6 +105,10 @@ class ControllerModuleIntarget extends Controller
             $data['projectId'] = '';
         }
 
+        $data['action'] = $this->url->link('module/'.$this->name, 'token=' . $this->session->data['token'], 'SSL');
+        $data['action_register'] = str_replace('&amp;', '&', $this->url->link('module/'.$this->name.'/register', 'token=' . $this->session->data['token'], 'SSL'));
+        $data['cancel'] = $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL');
+
         $data['header'] = $this->load->controller('common/header');
         $data['column_left'] = $this->load->controller('common/column_left');
         $data['footer'] = $this->load->controller('common/footer');
@@ -110,25 +116,23 @@ class ControllerModuleIntarget extends Controller
         $this->response->setOutput($this->load->view('module/intarget' . '.tpl', $data));
     }
 
-    protected function validate()
-    {
-        if (!$this->user->hasPermission('modify', 'module/intarget')) {
+    protected function validate() {
+        if (!$this->user->hasPermission('modify', 'module/'.$this->name)) {
             $this->error['warning'] = $this->language->get('error_permission');
         }
         return !$this->error;
     }
 
-    public function register()
-    {
+    public function register() {
 
         $json = array();
-        $this->load->language('module/intarget');
+        $this->load->language('module/'. $this->name);
 
-        $domain = 'intarget-dev.lembrd.com';
-        $settings = $this->request->post['intarget'];
-        $email = $settings['email'];
-        $key = $settings['key'];
-        $url = $settings['url'];
+        $domain = 'dev.intarget.ru';
+        $settings = $this->request->post[$this->name];
+        $email = $settings['email'];//'s-m-o-k@list.ru';
+        $key = $settings['key'];//'IolyfSM7oq3Ts62CPL7QCjou0hfmna8R';
+        $url = $settings['url'];//'http://opencart20.orcart.ru';
 
         if (($email == '') OR ($key == '')) {
             $json['result'] = 'error';
@@ -138,27 +142,32 @@ class ControllerModuleIntarget extends Controller
             $this->response->setOutput(json_encode($json));
             return;
         }
+
         $ch = curl_init();
+
         $jsondata = json_encode(array(
-                'email' => $email,
-                'key' => $key,
-                'url' => $url,
-                'cms' => 'opencart')
-        );
+            'email' => $email,
+            'key' => $key,
+            'url' => $url));
+
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Accept: application/json'));
+
         curl_setopt($ch, CURLOPT_URL, "http://" . $domain . "/api/registration.json");
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $jsondata);
+
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
         $server_output = curl_exec($ch);
+
         $json_result = json_decode($server_output);
         if (isset($json_result->status)) {
             if (($json_result->status == 'OK') && (isset($json_result->payload))) {
-                if (isset($json_result->payload->projectId)) {
+                if (isset($json_result->payload->projectId)){
                     $settings['projectId'] = $json_result->payload->projectId;
 
                     $this->load->model('setting/setting');
-                    $this->model_setting_setting->editSetting('intarget', array('intarget' => $settings));
+                    $this->model_setting_setting->editSetting($this->name, array($this->name => $settings));
 
                     $json['result'] = 'success';
                     $json['text'] = sprintf($this->language->get('text_register_success'), $json_result->payload->projectId);
@@ -166,33 +175,33 @@ class ControllerModuleIntarget extends Controller
                     $json['code'] = $json_result->code;
 
                 }
-                $query = $this->db->query("SELECT value FROM " . DB_PREFIX . "setting WHERE `key`='config_google_analytics'");
-                $intarget_code = $query->row['value'];
-                if (!strpos($intarget_code, 'INTARGET CODE START')) {
-                    $intarget_code .= "
-                        &lt;!-- INTARGET CODE START --&gt;
-                          &lt;script type=&quot;text/javascript&quot;&gt;
-                            (function(d, w, c) {
-                              w[c] = {
-                                projectId: " . $json_result->payload->projectId . "
-                              };
+                $query = $this->db->query("SELECT value FROM ". DB_PREFIX ."setting WHERE `key`='config_google_analytics'");
+                $google_code = $query->row['value'];
+                if (!strpos($google_code, 'INTARGET CODE START')) {
+                    $google_code .= "
+&lt;!-- INTARGET CODE START --&gt;
+  &lt;script type=&quot;text/javascript&quot;&gt;
+    (function(d, w, c) {
+      w[c] = {
+        projectId: ".$json_result->payload->projectId."
+      };
 
-                              var n = d.getElementsByTagName(&quot;script&quot;)[0],
-                              s = d.createElement(&quot;script&quot;),
-                              f = function () { n.parentNode.insertBefore(s, n); };
-                              s.type = &quot;text/javascript&quot;;
-                              s.async = true;
-                              s.src = &quot;//intarget-dev.lembrd.com/utlanalytics/loader.js&quot;;
+      var n = d.getElementsByTagName(&quot;script&quot;)[0],
+      s = d.createElement(&quot;script&quot;),
+      f = function () { n.parentNode.insertBefore(s, n); };
+      s.type = &quot;text/javascript&quot;;
+      s.async = true;
+      s.src = &quot;//dev.lembrd.com/utlanalytics/loader.js&quot;;
 
-                              if (w.opera == &quot;[object Opera]&quot;) {
-                                d.addEventListener(&quot;DOMContentLoaded&quot;, f, false);
-                              } else { f(); }
+      if (w.opera == &quot;[object Opera]&quot;) {
+        d.addEventListener(&quot;DOMContentLoaded&quot;, f, false);
+      } else { f(); }
 
-                            })(document, window, &quot;inTargetInit&quot;);
-                          &lt;/script&gt;
-                            &lt;!-- INTARGET CODE END --&gt;
-                          ";
-                    $this->db->query("UPDATE " . DB_PREFIX . "setting SET `value`='" . $intarget_code . "' WHERE `key`='config_google_analytics'");
+    })(document, window, &quot;inTargetInit&quot;);
+  &lt;/script&gt;
+    &lt;!-- INTARGET CODE END --&gt;
+  ";
+                    $this->db->query("UPDATE ". DB_PREFIX ."setting SET `value`='". $google_code ."' WHERE `key`='config_google_analytics'");
                 }
 
             } elseif ($json_result->status = 'error') {
@@ -207,26 +216,25 @@ class ControllerModuleIntarget extends Controller
 
     }
 
-    public function uninstall()
-    {
-        $query = $this->db->query("SELECT value FROM " . DB_PREFIX . "setting WHERE `key`='config_google_analytics'");
-        $intarget_code = $query->row['value'];
+    public function uninstall() {
+        $query = $this->db->query("SELECT value FROM ". DB_PREFIX ."setting WHERE `key`='config_google_analytics'");
+        $google_code = $query->row['value'];
         $start_text = '&lt;!-- INTARGET CODE START --&gt;';
         $end_text = '&lt;!-- INTARGET CODE END --&gt;';
-        $new_intarget_code = '';
-        $start = strpos($intarget_code, $start_text);
+        $start = 0;
+        $end = 0;
+        $new_google_code = '';
+        $start = strpos($google_code, $start_text);
         if ($start !== false) {
-            if ($start >= 2)
-                $start -= 2;
-            $end = strpos($intarget_code, $end_text);
+            if ($start >= 2) $start -= 2;
+            $end = strpos($google_code, $end_text);
             if ($end !== false) {
                 $end += strlen($end_text);
-                $new_intarget_code .= substr($intarget_code, 0, $start);
-                $new_intarget_code .= substr($intarget_code, $end);
-                $this->db->query("UPDATE " . DB_PREFIX . "setting SET `value`='" . $new_intarget_code . "' WHERE `key`='config_google_analytics'");
+                $new_google_code .= substr($google_code, 0, $start);
+                $new_google_code .= substr($google_code, $end);
+                $this->db->query("UPDATE ". DB_PREFIX ."setting SET `value`='". $new_google_code ."' WHERE `key`='config_google_analytics'");
             }
         }
     }
 }
-
 ?>
